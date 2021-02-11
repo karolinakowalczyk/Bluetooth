8062760ec49b7ab6399c9a5879612dcc12153500
Karolina <karolinakowalczyk1999@gmail.com>
2021-02-11 22:43:44 +0100

    Dodaj pliki projektów.

---
+++ lab4/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(204, 388);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(553, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Szukaj urządzeń Bluetooth";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(553, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(191, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Paruj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(553, 134);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(191, 34);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Prześlij plik";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Nazwa urządzenia";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(356, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(222, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Adres MAC";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(356, 60);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(152, 22);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(222, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "RSSI";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(356, 93);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(152, 22);
+            this.textBox4.TabIndex = 14;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(238, 235);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(531, 84);
+            this.listBox2.TabIndex = 15;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(238, 354);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(550, 84);
+            this.listBox3.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(235, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 17);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Logowane zdarzenia";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(235, 324);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 17);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Lista plików";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(554, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 26);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Dodaj plik do listy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(554, 174);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(189, 37);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "Wyczyść listę plików";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ lab4/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using Brecham.Obex;
+using System.Threading;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        BluetoothDeviceInfo[] _devices;
+        BluetoothDeviceInfo chosenDevice;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Wyszukwanie urządzenia
+        public void searchDevices()
+        {
+            BluetoothClient client = new BluetoothClient();
+            _devices = client.DiscoverDevices();
+
+            foreach (BluetoothDeviceInfo device in _devices)
+            {
+                listBox1.Items.Add(device.DeviceName + "(" + device.DeviceAddress + ")" + "\u000D\u000A");
+            }
+            
+
+        }
+
+        //Parowanie
+        private void pairDevice()
+        {
+            try
+            {
+                chosenDevice = _devices[listBox1.SelectedIndex];
+                BluetoothSecurity.PairRequest(chosenDevice.DeviceAddress, null);
+                button3.Enabled = true;
+                button4.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("Nie można połączyć się z " + chosenDevice.DeviceName.ToString());
+            }
+
+        }
+
+        private void fileOpener()
+        {
+            bool exist = false;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.ShowDialog();
+
+            foreach (String fileName in openFileDialog1.FileNames)
+            {
+                if (fileName != null)
+                {
+                    foreach (string item in listBox3.Items)
+                        if (item == fileName)
+                            exist = true;
+                }
+
+                if (!exist)
+                {
+                    listBox3.Items.Add(fileName);
+                }
+                else
+                {
+                    MessageBox.Show("Wybrany plik jest już na liście.");
+                }
+            }
+        }
+
+        private void SendFiles(object o)
+        {
+            foreach (string fileName in listBox3.Items)
+                {
+                    try
+                    {
+                        
+                        SynchronizationContext cntx = o as SynchronizationContext;
+                        cntx.Send(UpdateStartSendingMess, fileName);
+                        Uri uri = new Uri("obex://" + chosenDevice.DeviceAddress.ToString() + "/" + fileName);
+                        ObexWebRequest request = new ObexWebRequest(uri);
+                        request.ReadFile(fileName);
+                        ObexWebResponse response = (ObexWebResponse)request.GetResponse();
+                        cntx.Send(UpdateCorrectSendingMess, response.StatusCode);
+                    }
+                    catch (Exception error)
+                    {
+                    SynchronizationContext cntxerr = o as SynchronizationContext;
+                    String mess = error.Message;
+                    cntxerr.Send(UpdateErrorSendingMess, mess);
+                }
+                }
+            
+        }
+
+        private void UpdateStartSendingMess(object fileName)
+        {
+            listBox2.Items.Add("Rozpoczęto wysyłanie.\r\n");
+        }
+
+        private void UpdateCorrectSendingMess(object response)
+        {
+            listBox2.Items.Add("Odpowiedz serwera:" + response + "\r\n");
+        }
+
+        private void UpdateErrorSendingMess(object mess)
+        {
+            listBox2.Items.Add("Wystapił błąd! " + mess + "\r\n");
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            searchDevices();
+            button1.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pairDevice();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                String text = listBox1.SelectedItem.ToString();
+                chosenDevice = _devices[listBox1.SelectedIndex];
+                textBox2.Text = chosenDevice.DeviceName.ToString();
+                textBox3.Text = chosenDevice.DeviceAddress.ToString();
+                textBox4.Text = chosenDevice.Rssi.ToString();
+                button2.Enabled = true;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox3.Items.Count == 0)
+            {
+                listBox2.Items.Add("Brak plików!" + "\r\n");
+            }
+            else
+            {
+                Thread sendFilesthr = new Thread(SendFiles);
+                sendFilesthr.Start(SynchronizationContext.Current);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            fileOpener();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+        }
+    }
+}
